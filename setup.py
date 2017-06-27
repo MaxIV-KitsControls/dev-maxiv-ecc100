05c4270a141c9626b422202a755cc6fe799efcb4
Mikel Eguiraun <mikel.eguiraun@maxiv.lu.se>
2017-06-27 16:10:08 +0200

    v 0.1.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name = "tangods-ecc100",
-      version = "0.1.1",
+      version = "0.1.2",
       description = "Device server for the Attopcube ECC100",
       author = "Mikel Eguiraun",
       author_email = "mikel.eguiraun@maxiv.lu.se",
