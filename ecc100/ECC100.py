082643f44b2e86e4827e663826c9f7a8c10dbc35
Mikel Eguiraun <mikel.eguiraun@maxiv.lu.se>
2018-02-15 16:10:13 +0100

    working with referencing

--- ecc100/ECC100.py
+++ ecc100/ECC100.py
@@ -84,6 +84,13 @@ class ECC100 (PyTango.Device_4Impl):
         self.attr_Axis0Connected_read = False
         self.attr_Axis1Connected_read = False
         self.attr_Axis2Connected_read = False
+        self.attr_Axis0ReferencePosition_read = 0.0
+        self.attr_Axis1ReferencePosition_read = 0.0
+        self.attr_Axis2ReferencePosition_read = 0.0
+        self.attr_MovementDelay_read = 1.0
+        self.attr_Axis0ReferencePositionValid_read = False
+        self.attr_Axis1ReferencePositionValid_read = False
+        self.attr_Axis2ReferencePositionValid_read = False
         #----- PROTECTED REGION ID(ECC100.init_device) ENABLED START -----#
         self.ecc100 = libecc100.libECC100.ECC100(self.Host)
         self.set_state(PyTango.DevState.ON)
@@ -110,6 +117,7 @@ class ECC100 (PyTango.Device_4Impl):
         self.debug_stream("In write_PositionAxis0()")
         data = attr.get_write_value()
         #----- PROTECTED REGION ID(ECC100.PositionAxis0_write) ENABLED START -----#
+        self.ecc100.enable_movement(0)
         self.ecc100.set_position(0, int(data * 1e6))
         #----- PROTECTED REGION END -----#	//	ECC100.PositionAxis0_write
         
@@ -124,6 +132,7 @@ class ECC100 (PyTango.Device_4Impl):
         self.debug_stream("In write_PositionAxis1()")
         data = attr.get_write_value()
         #----- PROTECTED REGION ID(ECC100.PositionAxis1_write) ENABLED START -----#
+        self.ecc100.enable_movement(1)
         self.ecc100.set_position(1, int(data * 1e6))
         #----- PROTECTED REGION END -----#	//	ECC100.PositionAxis1_write
         
@@ -138,6 +147,7 @@ class ECC100 (PyTango.Device_4Impl):
         self.debug_stream("In write_PositionAxis2()")
         data = attr.get_write_value()
         #----- PROTECTED REGION ID(ECC100.PositionAxis2_write) ENABLED START -----#
+        self.ecc100.enable_movement(2)
         self.ecc100.set_position(2, int(data * 1e6))
         #----- PROTECTED REGION END -----#	//	ECC100.PositionAxis2_write
         
@@ -165,6 +175,80 @@ class ECC100 (PyTango.Device_4Impl):
         attr.set_value(self.attr_Axis2Connected_read)
         #----- PROTECTED REGION END -----#	//	ECC100.Axis2Connected_read
         
+    def read_Axis0ReferencePosition(self, attr):
+        self.debug_stream("In read_Axis0ReferencePosition()")
+        #----- PROTECTED REGION ID(ECC100.Axis0ReferencePosition_read) ENABLED START -----#
+        if self.attr_Axis0Connected_read & self.attr_Axis0ReferencePositionValid_read:
+            self.attr_Axis0ReferencePosition_read = self.ecc100.get_reference_position(0) / 1e6
+            attr.set_value(self.attr_Axis0ReferencePosition_read)
+        else:
+            attr.set_value(0)
+        #----- PROTECTED REGION END -----#	//	ECC100.Axis0ReferencePosition_read
+        
+    def read_Axis1ReferencePosition(self, attr):
+        self.debug_stream("In read_Axis1ReferencePosition()")
+        #----- PROTECTED REGION ID(ECC100.Axis1ReferencePosition_read) ENABLED START -----#
+        if self.attr_Axis1Connected_read & self.attr_Axis1ReferencePositionValid_read:
+            self.attr_Axis1ReferencePosition_read = self.ecc100.get_reference_position(1) / 1e6
+            attr.set_value(self.attr_Axis1ReferencePosition_read)
+        else:
+            attr.set_value(0)
+        #----- PROTECTED REGION END -----#	//	ECC100.Axis1ReferencePosition_read
+        
+    def read_Axis2ReferencePosition(self, attr):
+        self.debug_stream("In read_Axis2ReferencePosition()")
+        #----- PROTECTED REGION ID(ECC100.Axis2ReferencePosition_read) ENABLED START -----#
+        if self.attr_Axis2Connected_read & self.attr_Axis2ReferencePositionValid_read:
+            self.attr_Axis2ReferencePosition_read = self.ecc100.get_reference_position(2) / 1e6
+            attr.set_value(self.attr_Axis2ReferencePosition_read)
+        else:
+            attr.set_value(0)
+        #----- PROTECTED REGION END -----#	//	ECC100.Axis2ReferencePosition_read
+        
+    def read_MovementDelay(self, attr):
+        self.debug_stream("In read_MovementDelay()")
+        #----- PROTECTED REGION ID(ECC100.MovementDelay_read) ENABLED START -----#
+        attr.set_value(self.attr_MovementDelay_read)
+        
+        #----- PROTECTED REGION END -----#	//	ECC100.MovementDelay_read
+        
+    def write_MovementDelay(self, attr):
+        self.debug_stream("In write_MovementDelay()")
+        data = attr.get_write_value()
+        #----- PROTECTED REGION ID(ECC100.MovementDelay_write) ENABLED START -----#
+        self.attr_MovementDelay_read = data
+        #----- PROTECTED REGION END -----#	//	ECC100.MovementDelay_write
+        
+    def read_Axis0ReferencePositionValid(self, attr):
+        self.debug_stream("In read_Axis0ReferencePositionValid()")
+        #----- PROTECTED REGION ID(ECC100.Axis0ReferencePositionValid_read) ENABLED START -----#
+        if self.attr_Axis0Connected_read:
+            self.attr_Axis0ReferencePositionValid_read = bool(self.ecc100.get_reference_position_valid(0))
+            attr.set_value(self.attr_Axis0ReferencePositionValid_read)
+        else:
+            attr.set_value(False)
+        #----- PROTECTED REGION END -----#	//	ECC100.Axis0ReferencePositionValid_read
+        
+    def read_Axis1ReferencePositionValid(self, attr):
+        self.debug_stream("In read_Axis1ReferencePositionValid()")
+        #----- PROTECTED REGION ID(ECC100.Axis1ReferencePositionValid_read) ENABLED START -----#
+        if self.attr_Axis1Connected_read:
+            self.attr_Axis1ReferencePositionValid_read = bool(self.ecc100.get_reference_position_valid(1))
+            attr.set_value(self.attr_Axis1ReferencePositionValid_read)
+        else:
+            attr.set_value(False)  
+        #----- PROTECTED REGION END -----#	//	ECC100.Axis1ReferencePositionValid_read
+        
+    def read_Axis2ReferencePositionValid(self, attr):
+        self.debug_stream("In read_Axis2ReferencePositionValid()")
+        #----- PROTECTED REGION ID(ECC100.Axis2ReferencePositionValid_read) ENABLED START -----#
+        if self.attr_Axis2Connected_read:
+            self.attr_Axis2ReferencePositionValid_read = bool(self.ecc100.get_reference_position_valid(2))
+            attr.set_value(self.attr_Axis2ReferencePositionValid_read)
+        else:
+            attr.set_value(False)
+        #----- PROTECTED REGION END -----#	//	ECC100.Axis2ReferencePositionValid_read
+        
     
     
             
@@ -188,6 +272,66 @@ class ECC100 (PyTango.Device_4Impl):
         #----- PROTECTED REGION ID(ECC100.ResetPosition) ENABLED START -----#
         self.ecc100.reset_position(argin)
         #----- PROTECTED REGION END -----#	//	ECC100.ResetPosition
+        
+    def TimedContinousForward(self, argin):
+        """ 
+        :param argin: axis
+        :type argin: PyTango.DevLong
+        """
+        self.debug_stream("In TimedContinousForward()")
+        #----- PROTECTED REGION ID(ECC100.TimedContinousForward) ENABLED START -----#
+        self.ecc100.timed_continous_fordward(argin, self.attr_MovementDelay_read)
+        #----- PROTECTED REGION END -----#	//	ECC100.TimedContinousForward
+        
+    def TimedContinousBackward(self, argin):
+        """ 
+        :param argin: axis,delay
+        :type argin: PyTango.DevLong
+        """
+        self.debug_stream("In TimedContinousBackward()")
+        #----- PROTECTED REGION ID(ECC100.TimedContinousBackward) ENABLED START -----#
+        self.ecc100.timed_continous_backward(argin, self.attr_MovementDelay_read)
+        #----- PROTECTED REGION END -----#	//	ECC100.TimedContinousBackward
+        
+    def EnableMovement(self, argin):
+        """ 
+        :param argin: axis
+        :type argin: PyTango.DevLong
+        """
+        self.debug_stream("In EnableMovement()")
+        #----- PROTECTED REGION ID(ECC100.EnableMovement) ENABLED START -----#
+        self.ecc100.enable_movement(argin)
+        #----- PROTECTED REGION END -----#	//	ECC100.EnableMovement
+        
+    def DisableMovement(self, argin):
+        """ 
+        :param argin: axis
+        :type argin: PyTango.DevLong
+        """
+        self.debug_stream("In DisableMovement()")
+        #----- PROTECTED REGION ID(ECC100.DisableMovement) ENABLED START -----#
+        self.ecc100.disable_movement(argin)
+        #----- PROTECTED REGION END -----#	//	ECC100.DisableMovement
+        
+    def EnableOutputRelay(self, argin):
+        """ 
+        :param argin: axis
+        :type argin: PyTango.DevLong
+        """
+        self.debug_stream("In EnableOutputRelay()")
+        #----- PROTECTED REGION ID(ECC100.EnableOutputRelay) ENABLED START -----#
+        self.ecc100.enable_output_relay(argin)
+        #----- PROTECTED REGION END -----#	//	ECC100.EnableOutputRelay
+        
+    def DisableOutputRelay(self, argin):
+        """ 
+        :param argin: axis
+        :type argin: PyTango.DevLong
+        """
+        self.debug_stream("In DisableOutputRelay()")
+        #----- PROTECTED REGION ID(ECC100.DisableOutputRelay) ENABLED START -----#
+        self.ecc100.disable_output_relay(argin)
+        #----- PROTECTED REGION END -----#	//	ECC100.DisableOutputRelay
         
 
     #----- PROTECTED REGION ID(ECC100.programmer_methods) ENABLED START -----#
@@ -220,6 +364,24 @@ class ECC100Class(PyTango.DeviceClass):
         'ResetPosition':
             [[PyTango.DevULong, "none"],
             [PyTango.DevVoid, "none"]],
+        'TimedContinousForward':
+            [[PyTango.DevLong, "axis"],
+            [PyTango.DevVoid, "none"]],
+        'TimedContinousBackward':
+            [[PyTango.DevLong, "axis,delay"],
+            [PyTango.DevVoid, "none"]],
+        'EnableMovement':
+            [[PyTango.DevLong, "axis"],
+            [PyTango.DevVoid, "none"]],
+        'DisableMovement':
+            [[PyTango.DevLong, "axis"],
+            [PyTango.DevVoid, "none"]],
+        'EnableOutputRelay':
+            [[PyTango.DevLong, "axis"],
+            [PyTango.DevVoid, "none"]],
+        'DisableOutputRelay':
+            [[PyTango.DevLong, "axis"],
+            [PyTango.DevVoid, "none"]],
         }
 
 
@@ -228,15 +390,27 @@ class ECC100Class(PyTango.DeviceClass):
         'PositionAxis0':
             [[PyTango.DevDouble,
             PyTango.SCALAR,
-            PyTango.READ_WRITE]],
+            PyTango.READ_WRITE],
+            {
+                'unit': "um",
+                'format': "%6.6f",
+            } ],
         'PositionAxis1':
             [[PyTango.DevDouble,
             PyTango.SCALAR,
-            PyTango.READ_WRITE]],
+            PyTango.READ_WRITE],
+            {
+                'unit': "um",
+                'format': "%6.6f",
+            } ],
         'PositionAxis2':
             [[PyTango.DevDouble,
             PyTango.SCALAR,
-            PyTango.READ_WRITE]],
+            PyTango.READ_WRITE],
+            {
+                'unit': "um",
+                'format': "%6.6f",
+            } ],
         'Axis0Connected':
             [[PyTango.DevBoolean,
             PyTango.SCALAR,
@@ -246,6 +420,46 @@ class ECC100Class(PyTango.DeviceClass):
             PyTango.SCALAR,
             PyTango.READ]],
         'Axis2Connected':
+            [[PyTango.DevBoolean,
+            PyTango.SCALAR,
+            PyTango.READ]],
+        'Axis0ReferencePosition':
+            [[PyTango.DevDouble,
+            PyTango.SCALAR,
+            PyTango.READ],
+            {
+                'unit': "um",
+                'format': "%6.6f",
+            } ],
+        'Axis1ReferencePosition':
+            [[PyTango.DevDouble,
+            PyTango.SCALAR,
+            PyTango.READ],
+            {
+                'unit': "um",
+                'format': "%6.6f",
+            } ],
+        'Axis2ReferencePosition':
+            [[PyTango.DevDouble,
+            PyTango.SCALAR,
+            PyTango.READ],
+            {
+                'unit': "um",
+                'format': "%6.6f",
+            } ],
+        'MovementDelay':
+            [[PyTango.DevDouble,
+            PyTango.SCALAR,
+            PyTango.READ_WRITE]],
+        'Axis0ReferencePositionValid':
+            [[PyTango.DevBoolean,
+            PyTango.SCALAR,
+            PyTango.READ]],
+        'Axis1ReferencePositionValid':
+            [[PyTango.DevBoolean,
+            PyTango.SCALAR,
+            PyTango.READ]],
+        'Axis2ReferencePositionValid':
             [[PyTango.DevBoolean,
             PyTango.SCALAR,
             PyTango.READ]],
